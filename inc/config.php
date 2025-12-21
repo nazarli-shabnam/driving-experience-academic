267e74a3d7b9e489f60e73fc50b39bee977a25b9
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-21 20:09:37 +0400

    it werks. ship it

---
+++ inc/config.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Application configuration
+ * Single-user application
+ */
+
+session_start();
+
+/* Database configuration */
+define('DB_HOST', 'mysql-shabnam-nazarli.alwaysdata.net');
+define('DB_NAME', 'shabnam-nazarli_driving_experience');
+define('DB_USER', '410043_everyone');
+define('DB_PASS', 'Pass_word123');
+define('DB_PORT', '3306');
